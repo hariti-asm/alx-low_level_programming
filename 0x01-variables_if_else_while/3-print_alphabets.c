81dca401edc1f632ca3f747e8654552c834e40a7
hariti asmae <haritiasmaa74@gmail.com>
2023-03-18 07:52:53 -0700

    3

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main(void) 
+{
+/*  print lowercase alphabe */
+for (char c = 'a'; c <= 'z'; c++)
+{
+putchar(c);
+}
+
+/* print uppercase alphabet */
+for (char c = 'A'; c <= 'Z'; c++)
+putchar(c);
+/* print newline character */
+putchar('\n');
+return (0);
+
+}
+
