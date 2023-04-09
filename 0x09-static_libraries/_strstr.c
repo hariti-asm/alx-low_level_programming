9a076f1f50f5f936d32da2ffef64fc5ce24dac98
hariti asmae <72623740+hariti-asm@users.noreply.github.com>
2023-04-09 00:01:22 +0000

    Update _strstr.c

--- 0x09-static_libraries/_strstr.c
+++ 0x09-static_libraries/_strstr.c
@@ -1,5 +1,11 @@
 #include "main.h"
+/**
+ * _islower - checks if a character is lowercase
+ * @c: the character to check
+ *
+ * Return: 1 if c is lowercase, 0 otherwise
+ */
 char *_strstr(char *haystack, char *needle)
-{ /** it returns 0 */
+{ 
 	return (0);
 }
