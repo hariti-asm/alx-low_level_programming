a18d59fa0eb136091803881caa98a19992a8c7d5
hariti asmae <haritiasmaa74@gmail.com>
2023-04-09 09:58:44 -0700

    betty

--- 0x09-static_libraries/_strstr.c
+++ 0x09-static_libraries/_strstr.c
@@ -6,6 +6,11 @@
  * Return: 1 if c is lowercase, 0 otherwise
  */
 char *_strstr(char *haystack, char *needle)
-{ 
-	return (0);
+{/**
+ * _islower - checks if a character is lowercase
+ * @c: the character to check
+ *
+ * Return: 1 if c is lowercase, 0 otherwise
+ */ 
+return (0);
 }
